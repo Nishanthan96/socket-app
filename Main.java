57171ee9d420a6ad3422d0f0a13242e8a2d7ed33
Nishanthan96 <67302874+Nishanthan96@users.noreply.github.com>
2020-06-24 23:18:44 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,47 @@
-
-
 package com.company;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+
 import java.net.Socket;
 
+
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] args) {
-        BasicCrypto crypto = new BasicCrypto();
 
+        Crypto crypto=new BasicCrypto();
         try {
-            Socket s1 = new Socket("127.0.0.1", 1201);
+
+
+            Socket s1 = new Socket("127.0.0.7", 1001);
+
+
             DataInputStream din = new DataInputStream(s1.getInputStream());
             DataOutputStream dout = new DataOutputStream(s1.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String msgin = "";
-            String msgout = "";
 
-            while(!msgin.equalsIgnoreCase("end")) {
-                msgout = br.readLine();
-                String enc = new String(crypto.encrypt(msgout.getBytes()));
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String in = "", out = "";
+            do {
+
+
+                out = br.readLine();
+                String enc=new String(crypto.encrypt(out.getBytes())); //encrypting
+
                 System.out.println("Encrypted :" + enc);
                 dout.writeUTF(enc);
-                msgin = din.readUTF();
-                String dec = new String(crypto.decrypt(msgin.getBytes()));
-                System.out.println("Before decryption :" + msgin);
-                System.out.println("Friend:" + dec);
-            }
-        } catch (Exception var10) {
-        }
+                in = din.readUTF();
+                String dec=new String(crypto.decrypt(in.getBytes()));//decrypting
+                System.out.println("Before decryption :"+in);
+                System.out.println("Friend:" + dec);// print chat
 
+
+            }while (!out.equals("bye"));
+            System.out.println("Server closed");
+            s1.close();
+
+        } catch (Exception e) {
+            //Handle Exceptions
+        }
     }
 }
